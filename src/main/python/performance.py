f999e7a841ae4d89ad32c372572e244436b35670
jfuerth <jfuerth@21f3bb90-f8c0-11de-8173-5df4dc7aba59>
2010-01-22 15:26:41 +0000

    Updated copyright message and attached BSD license to all source files.

--- src/main/python/performance.py
+++ src/main/python/performance.py
@@ -1,5 +1,38 @@
 #!/usr/bin/python
 
+# 
+# performance.py
+# 
+# Copyright (c) 2010, Jonathan Fuerth
+# 
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the
+#       distribution.
+#     * Neither the name of Jonathan Fuerth nor the names of other
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 class Move:
     def __init__(self, fromh, jumped, to):
         self.fromh = fromh
@@ -108,7 +141,15 @@ class GameState:
             self.rowCount = initialState.rowCount
             self.occupiedHoles = []
             self.occupiedHoles.extend(initialState.occupiedHoles)
+
+            # XXX List.remove() and List.index() raise ValueError if
+            #     requested item is not present. Following code relies
+            #     on this because I couldn't find a better way.
             
+            # is this really the best/only way to check for existence
+            # of a list item? I'd expect use of exceptions for normal
+            # control of flow to be a no-no...
+
             self.occupiedHoles.remove(applyMe.fromh)
             self.occupiedHoles.remove(applyMe.jumped)
 
